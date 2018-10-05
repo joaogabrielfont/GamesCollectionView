bbb5438f0276ae5d88bfc87bd353807d2ef92638
João Gabriel <joao.conceicao@monitoratec.com.br>
2018-10-05 11:15:25 -0300

    Initial Commit

---
+++ GamesCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GamesCollectionView
+//
+//  Created by João Gabriel  on 05/10/2018.
+//  Copyright © 2018 fontana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
